53f3dcf728a9f4fd1b0cee0f11f3d2d4ff855235
Mihail Mikov <m.mikoff@gmail.com>
2025-10-17 01:58:45 +0300

    initial bootstrap & outline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://debel.github.io',
+  base: '/js-toolchain-evolution',
+  vite: {
+    plugins: [tailwind()],
+  }
+});
